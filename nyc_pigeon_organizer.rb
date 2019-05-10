0ab4a21221825da90d9a805bfcaed78eaa07d3ea
Jess Rezac <jess.rezac@gmail.com>
2019-05-10 16:43:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,9 +4,9 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(trait, variants), result|
     variants.each do |variant, birds|
       birds.each do |bird|
-        pigeon_list[bird] ||= {}
-        pigeon_list[bird][trait] ||= []
-        pigeon_list[bird][trait] << variant.to_s
+        result[bird] ||= {}
+        result[bird][trait] ||= []
+        result[bird][trait] << variant.to_s
       end
     end
   end
